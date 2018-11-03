7c80ff1485c6884a8135654f9181519fda9e0b87
Jovan Popovic <jocapc@gmail.com>
2018-11-04 00:54:11 +0100

    Splitting front-end and backend code

---
+++ src/backend-dotnet/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Belgrade.SqlClient;
+using Belgrade.SqlClient.SqlDb;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.SqlClient;
+
+namespace BackendApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string ConnString = Configuration["ConnectionStrings:HeroDb"];
+
+            if (ConnString == null)
+                throw new System.Exception("Cannot read config: " + ConnString);
+            services.AddTransient<IQueryPipe>(_ => new QueryPipe(new SqlConnection(ConnString)));
+            services.AddTransient<ICommand>(_ => new Command(new SqlConnection(ConnString)));
+
+            services.AddCors();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader().WithMethods("get", "post", "put","delete", "options"));
+
+            app.UseMvc();
+        }
+    }
+}
